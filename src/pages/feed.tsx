eaff504858c2282b40749bd3347a41f50e5d8762
8wraps <hachimakivape@gmail.com>
2023-10-27 20:10:31 +0900

    キャッシュ時間短くしてみる

--- src/pages/feed.tsx
+++ src/pages/feed.tsx
@@ -3,8 +3,9 @@ import type { GetServerSideProps } from 'next';
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   const xml = await generateRssFeed();
+  console.log(xml);
   res.statusCode = 200;
-  res.setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate'); // 24時間キャッシュする
+  res.setHeader('Cache-Control', 's-maxage=5, stale-while-revalidate'); // 24時間キャッシュする
   res.setHeader('Content-Type', 'text/xml');
   res.end(xml);
 
